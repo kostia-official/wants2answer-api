34606357c1f755288e72307fb641634fc66b668a
kozzztya <kozzztya@gmail.com>
2020-03-16 15:49:01 +0200

    Add temporary auth logs

--- src/hooks/authorization.js
+++ src/hooks/authorization.js
@@ -2,6 +2,9 @@ const { NotAuthenticated } = require('@feathersjs/errors');
 const config = require('config');
 
 module.exports = ctx => {
+  console.log('config', config.get('apiToken'));
+  console.log('params', ctx.params.token);
+
   if (config.get('apiToken') !== ctx.params.token) {
     throw new NotAuthenticated('Wrong API token!');
   }
