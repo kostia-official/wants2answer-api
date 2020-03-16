2681099b0fd796110e53a7b5b2260c23115f0886
kozzztya <kozzztya@gmail.com>
2020-03-16 15:58:19 +0200

    Remove auth logs

--- src/hooks/authorization.js
+++ src/hooks/authorization.js
@@ -2,9 +2,6 @@ const { NotAuthenticated } = require('@feathersjs/errors');
 const config = require('config');
 
 module.exports = ctx => {
-  console.log('config', config.get('apiToken'));
-  console.log('params', ctx.params.token);
-
   if (config.get('apiToken') !== ctx.params.token) {
     throw new NotAuthenticated('Wrong API token!');
   }
