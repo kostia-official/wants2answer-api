546708c9b2193d1049e9a1e3e0cf893373854f19
kozzztya <kozzztya@gmail.com>
2020-03-16 15:44:26 +0200

    Fix NotAuthenticated error message

--- src/hooks/authorization.js
+++ src/hooks/authorization.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 module.exports = ctx => {
   if (config.get('apiToken') !== ctx.params.token) {
-    throw new NotAuthenticated('No API token!');
+    throw new NotAuthenticated('Wrong API token!');
   }
 
   return ctx;
